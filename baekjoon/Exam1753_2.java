4ebf5726162bc1cc7fba58280d6e7d1088c5b03a
kimjongmo <kfmd1008@naver.com>
2018-02-10 16:10:14 +0900

    What does the priority queue compare to? equals? compareTo?

--- baekjoon/Exam1753_2.java
+++ baekjoon/Exam1753_2.java
@@ -4,6 +4,7 @@ import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Exam1753_2 {
+	public static final int INF = 200001;
 	static Scanner sc = new Scanner(System.in);
 	static Map<Integer, Line[]> map = new HashMap<>(); // 해당 간선의 리스트
 	static int totalV = 0;// 전체 정점의 갯수
@@ -18,14 +19,9 @@ public class Exam1753_2 {
 		Line[] lines = map.get(start);
 		
 		for(int i=1;i<=totalV;i++) {
-			if(lines[i]==null) {//값이 null => 현재 거리가 무한대인 정점을 의미.
-				continue;
-			}else if(i==start){//자기 자신은 볼 필요가 없음.
-				continue;
-			}else {				//값을 아는 것만 큐에 넣는다.
-				queue.add(lines[i]);
-			}
+			queue.add(lines[i]);
 		}
+		
 		return queue;
 	}
 
@@ -33,26 +29,33 @@ public class Exam1753_2 {
 		
 		queue = getQueue();//우선순위 큐 초기화
 		
-		while(queue.peek()!=null) {//큐에 더이상 값이 없을 때 까지 반복한다.
+		boolean[] visit = new boolean[totalV+1];	//방문 여부.
+		visit[start] = true;	//자기 자신 방문 완료
+		
+		while(!queue.isEmpty()) {//큐에 더이상 값이 없을 때 까지 반복한다.
 			
-			Line line = queue.poll();	//큐에서 하나를 반환(=거리가 가장 최소).
+			int minV = queue.poll().v;	//거리가 가장 가까운 정점
 			
-			Line[] lines1 = map.get(line.v);	//해당 정점의 라인들을 불러온다.
-			Line[] lines2 = map.get(start);		
+			if(visit[minV]==true) {	//방문한 적이 있는 점이라면 패스
+				continue;
+			}else {						//방문한 적 없다면 방문을 한다.
+				visit[minV]= true;
+			}
+			
+			Line[] v1 = map.get(start);
+			Line[] v2 = map.get(minV);
+			
 			for(int i=1;i<totalV;i++) {
-				if(lines1[i]==null) {
-					continue;
-				}else if(i==line.v) {
-					continue;
-				}else {
-					if(lines2[i]!=null) {
-						lines2[i].w = Math.min(lines2[i].w,lines2[line.v].w + lines1[i].w);
-						//매번 큐를 업데이트 하자니.. 계속 같은 최소 정점만 나올테고.
-						//큐를 한번만 초기화 시키고 계속 쓰자니. 큐를 업데이트하는 것이 복잡하고
-						//진퇴양난이로다....
-					}
+				if(i==minV) continue;	//minV라인에서 minV는 0이므로 0으로 되버림.
+				if(v1[i].w > v1[minV].w+v2[i].w ) {
+					v1[i].w = v1[minV].w+v2[i].w;
+					queue.remove(new Line(i));
+					queue.add(new Line(i,v1[i].w));
 				}
 			}
+			
+			
+			
 		}
 		
 		
@@ -67,6 +70,9 @@ public class Exam1753_2 {
 			
 			for (int i = 1; i <= totalV; i++) {
 				Line[] lines = new Line[totalV+1];
+				for(int j=1;j<=totalV;j++) {
+					lines[j]=new Line(j);
+				}
 				lines[i]=new Line(i,0);
 				map.put(i, lines);
 			}
@@ -85,9 +91,11 @@ public class Exam1753_2 {
 			e.printStackTrace();
 		}
 
-		show();
-
-
+		//show();
+		
+		updateDistance();
+		
+		answer();
 
 	}
 
@@ -96,7 +104,7 @@ public class Exam1753_2 {
 			Line[] lines = map.get(i);
 			System.out.print(i + "\t");
 			for (int j = 1; j <= totalV; j++) {
-				if (lines[j]==null) {
+				if (lines[j].w==Exam1753_2.INF) {
 					System.out.print("INF\t");
 				} else {
 					System.out.print(lines[j].w + "\t");
@@ -105,11 +113,20 @@ public class Exam1753_2 {
 			System.out.println();
 		}
 	}
+	static void answer() {
+		Line[] line = map.get(start);
+		for(int i=1;i<=totalV;i++) {
+			if(line[i].w==Exam1753_2.INF) 
+				System.out.println("INF");
+			else
+				System.out.println(line[i].w);
+		}
+	}
 }
 
 class Line implements Comparable<Line> {
 	int v; // 정점
-	int w = 200001; // 간선 가중치 , 최대값 10*20,000를 넘지 못한다. 따라서 200001은 무한대를 의미함.
+	int w = Exam1753_2.INF; // 간선 가중치 , 최대값 10*20,000를 넘지 못한다. 따라서 200001은 무한대를 의미함.
 
 	public Line(int v) {
 		this.v = v;
@@ -119,7 +136,14 @@ class Line implements Comparable<Line> {
 		this.v = v;
 		this.w = w;
 	}
-
+	
+	public boolean equals(Line line) {
+		if(line.v == this.v) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 	public int compareTo(Line line) {
 		if (this.w == line.w) {
 			return 0;
@@ -131,3 +155,4 @@ class Line implements Comparable<Line> {
 	}
 }
 // 5 6 1 5 1 1 1 2 2 1 3 3 2 3 4 2 4 5 3 4 6
+// 6 9 1 1 6 14 1 2 7 1 3 9 2 3 10 2 4 15 3 6 2 3 4 11 6 5 9 4 5 6
